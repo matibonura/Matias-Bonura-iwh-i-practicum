17cf54c29370d5c523053691979e3794c8e60980
Matias Bonura <matiasoyarse36@gmail.com>
2025-04-09 11:09:50 -0300

    Update for custom objects

--- index.js
+++ index.js
@@ -2,25 +2,63 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 
+require('dotenv').config();
+
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+
+const PRIVATE_APP_ACCESS = process.env.HUBSPOT_API_KEY;
+const OBJECT_TYPE = 'programa_estudiantil';
+const API_URL = `https://api.hubapi.com/crm/v3/objects/${OBJECT_TYPE}`;
+const HEADERS = {
+  Authorization: `Bearer ${HUBSPOT_API_KEY}`,
+  'Content-Type': 'application/json'
+};
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+    try {
+      const response = await axios.get(`${API_URL}?properties=propiedad1,propiedad2,propiedad3`, { headers: HEADERS });
+      const data = response.data.results;
+      res.render('homepage', { title: 'Homepage | HubSpot I Practicum', data });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Error al cargar los registros');
+    }
+  });
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
-// * Code for Route 2 goes here
+app.get('/update-cobj', (req, res) => {
+    res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum' });
+  });
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
-// * Code for Route 3 goes here
+app.post('/update-cobj', async (req, res) => {
+    const { prop1, prop2, prop3 } = req.body;
+  
+    const newRecord = {
+      properties: {
+        Nombre: nombre_del_programa,
+        Estado: estado,
+        Modalidad: modalidad
+      }
+    };
+  
+    try {
+      await axios.post(API_URL, newRecord, { headers: HEADERS });
+      res.redirect('/');
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Error al crear el registro');
+    }
+  });
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
